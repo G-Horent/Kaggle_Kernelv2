39ae2ae796f4514cccc914a209763e764f8b184f
Buecher <leobuecher@orange.fr>
2023-04-11 18:02:44 +0200

    Code to tackle errors in RWKernel computations, & Balanced SVM

--- kernel_methods.py
+++ kernel_methods.py
@@ -12,7 +12,9 @@ def get_kernel(name, **kwargs):
     elif name == 'Kernel_nwalk':
         return Kernel_nwalk(n=kwargs['n'])
     elif name == 'RandomWalkKernel':
-        return RandomWalkKernel(lam=kwargs['lam_rand_walk'])
+        adapted_kwargs = {k: v for (k,v) in kwargs.items() if k!='rwlam'}
+        adapted_kwargs['lam'] = kwargs['rwlam']
+        return RandomWalkKernel(lam=kwargs['rwlam'])
     else:
         raise NotImplementedError('Unknown kernel')
 
@@ -60,21 +62,25 @@ class KernelLogisticRegression:
 
 
 class KernelSVM(KernelMethod):
-    def __init__(self, lmbd=1., kernel_name='KernelRBF', precomputed_kernel=False, kernel_path='saved/', **kwargs):
+    def __init__(self, lmbd=1., kernel_name='KernelRBF', precomputed_kernel=False, balanced = False, kernel_path='saved/', **kwargs):
         super().__init__(kernel_name=kernel_name, **kwargs)
         self.lmbd = lmbd
+        self.balanced = balanced
         # self.kernel = kernel
-        self.alpha = None
-        self.params = None
+        self.precomputed_kernel = precomputed_kernel
+        self.kernel_path = kernel_path
+        # self.params = None #not used
+
         self.X_support = None
         self.X_mean = None
         self.X_std = None
-        self.precomputed_kernel = precomputed_kernel
-        self.kernel_path = kernel_path
+
+        self.alpha = None
         self.alpha_support = None
 
     def fit(self, graph_list_train, y):
         N = graph_list_train.shape[0]
+
         if self.kernel.name == "KernelRBF":
             # If Kernel RBF, compute feature matrix
             X_train = self.kernel.extract_features(graph_list_train)
@@ -96,11 +102,18 @@ class KernelSVM(KernelMethod):
             print(np.linalg.eigh(K))
         else:
             K = np.load(self.kernel_path)
+        
+        if self.balanced:
+            N_pos = np.count_nonzero(y==1)
+            weights = np.where(y==1, N/(2*N_pos), N/(2*(N - N_pos)))
+        else:
+            weights = np.ones((N))
 
         print("Fitting KernelSVM")
         alpha = cp.Variable(N)
-        obj = cp.Maximize(2 * alpha.T @ y - cp.quad_form(alpha, cp.psd_wrap(K)))
-        constraints = [0 <= cp.multiply(y, alpha), cp.multiply(y, alpha) <= 1 / (2 * self.lmbd * N)]
+        # obj = cp.Maximize(2 * alpha.T @ y - cp.quad_form(alpha, cp.psd_warp(K)))
+        obj = cp.Maximize(2 * alpha.T @ y - cp.quad_form(alpha, K))
+        constraints = [0 <= cp.multiply(y, alpha), cp.multiply(y, alpha) <= weights / (2 * self.lmbd * N)]
         start = time()
         prob = cp.Problem(obj, constraints)
         result = prob.solve()
@@ -113,22 +126,32 @@ class KernelSVM(KernelMethod):
         self.alpha_support = np.copy(self.alpha[idx_alpha_support])
         self.X_support = np.copy(X_train[idx_alpha_support])
 
-    def predict(self, graph_list_test):
-        if self.kernel.name == 'KernelRBF':
-            X_test = self.kernel.extract_features(graph_list_test)
-            X_test = (X_test - self.X_mean) / self.X_std
+    def predict(self, graph_list_test, precomputed=False, kernel_outer_path = "saved/"):
+        if not precomputed:
+            if self.kernel.name == 'KernelRBF':
+                X_test = self.kernel.extract_features(graph_list_test)
+                X_test = (X_test - self.X_mean) / self.X_std
 
+            else:
+                X_test = graph_list_test
+
+            print("Computing kernel_outer")
+            kernel_outer = self.kernel.compute_outer_gram(X_test, self.X_support)
+            return kernel_outer @ self.alpha_support
+        
         else:
-            X_test = graph_list_test
+            kernel_outer = np.load(kernel_outer_path)
+            return kernel_outer @ self.alpha
 
-        print("Computing kernel_outer")
-        kernel_outer = self.kernel.compute_outer_gram(X_test, self.X_support)
-        return kernel_outer @ self.alpha_support
+    def score(self, X, y, precomputed=False, kernel_outer_path = "saved/", score_type='accuracy'):
+        y_pred = self.predict(X, precomputed=precomputed, kernel_outer_path=kernel_outer_path)
 
-    def score(self, X, y):
-        y_pred = self.predict(X)
-
-        return np.sum(np.sign(y_pred) == y) / y.shape[0]
+        if score_type == 'AUROC':
+            auc = np.count_nonzero(y_pred[y == 1][:,None] > y_pred[y != 1][None, :])
+            auc /= np.count_nonzero(y == 1)*np.count_nonzero(y != 1)
+            return auc
+        else:
+            return np.sum(np.sign(y_pred) == y) / y.shape[0]
 
 
 if __name__ == '__main__':
