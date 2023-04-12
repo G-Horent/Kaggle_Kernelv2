64fff42c22df3703069dc279c649e991f6e54982
Buecher <leobuecher@orange.fr>
2023-04-12 17:58:45 +0200

    New SVM

--- analysis_kernels.py
+++ analysis_kernels.py
@@ -4,14 +4,95 @@ from data import load_training_data, load_test_data
 
 def normalize_kernels(path):
     ker = np.load(path)
-    ker_normalized = np.zeros_like(ker)
 
-    for i in range(ker.shape[0]):
-        for j in range(ker.shape[1]):
-            ker_normalized[i, j] = ker[i, j] / (np.sqrt(ker[i, i] * ker[j, j] + 1e-8))
+    # ker_normalized = np.zeros_like(ker)
+    # for i in range(ker.shape[0]):
+    #     for j in range(ker.shape[1]):
+    #         ker_normalized[i, j] = ker[i, j] / (np.sqrt(ker[i, i] * ker[j, j] + 1e-8))
+    
+    ker_normalized = normalize(ker, eps=1e-8)
 
     np.save(path.replace('.npy', '_normalized.npy'), ker_normalized)
 
+def normalize(K, eps=0):
+    K_diag = K[np.arange(K.shape[0]), np.arange(K.shape[0])]
+    K_norm = K/np.sqrt(K_diag[:,None] * K_diag[None,:] + eps)
+    return K_norm
+
+def psdfy(K, eps = 0):
+    w, v = np.linalg.eigh(K)
+    w_pos_part = np.maximum(eps, w)
+    K_psd = (v*w_pos_part)@v.T
+    K_psd = (K_psd + K_psd.T)/2
+    return K_psd
+
+def correct_kernel(K, slow_kernel, sub_graphs1, sub_graphs2 = None, tol=0.01, stop_cond=10):
+    """Correct absurd values with a slower using surer kernel. Starts with nan, then higher values in absolute values.
+
+    Args:
+        K (ndarray): Kernel matrix to correct
+        slow_kernel (Kernel): Kernel object, with option fast=False
+        sub_graphs1 (list): List of (processed) graphs corresponding to rows
+        sub_graphs2 (list, optional): List of (processed) graphs corresponding to columns. If None, assumed to be equal to sub_graphs1. Defaults to None.
+        tol (float, optional): When a correction leads to a relative difference < tol, then we consider the initial value was good. Defaults to 0.01.
+        stop_cond (int, optional): Number of consecutive times the initial value must be good to decide to stop the algorithm. Defaults to 10.
+
+    Returns:
+        ndarray: Corrected kernel matrix
+        list: list of old values replaced (in the corresponding order)
+        list: list of new values old values have been replaced by (in the corresponding order)
+        list: list of difference new - old values (in the corresponding order)
+    """ 
+    n = K.shape[0]
+    K_correct = np.copy(K)
+    inner = sub_graphs2 is None
+    if inner: sub_graphs2 = sub_graphs1
+    assert K.shape[0] == len(sub_graphs1)
+    assert K.shape[1] == len(sub_graphs2)
+
+    idx_big = np.argsort(np.abs(K).flatten(order='C'))[::-1]
+    # nan are at the beginning
+    i_big = idx_big//n
+    j_big = idx_big%n
+
+    if inner:
+        # To remove the lower triangular part
+        of_interest = j_big >= i_big
+        i_big = i_big[of_interest]
+        j_big = j_big[of_interest]
+    
+    k_old_list = []
+    k_cor_list = []
+    k_diff_list = []
+    is_tol_list = []
+    for q in range(len(i_big)):
+        i, j = i_big[q], j_big[q]
+        k_cor, info = slow_kernel.kernel_eval(sub_graphs1[i], sub_graphs2[j])
+        k_old = K[i, j]
+        k_diff = k_cor - k_old
+        is_tol = not(np.isnan(k_old)) and (np.abs(k_diff) < tol*np.abs(k_old))
+
+        if q%100 == 0 or is_tol: 
+            print(f"{q}: {k_cor} {k_old} {k_diff}              ", end="\r")
+
+        k_old_list.append(k_old)
+        k_cor_list.append(k_cor)
+        k_diff_list.append(k_diff)
+        is_tol_list.append(is_tol)
+
+        # if is_tol:
+        #     cntn = input(f"{q} {k_cor} {k_old} {k_diff}")
+        #     if cntn=="stop":
+        #         break
+        
+        K_correct[i, j] = k_cor
+        if inner:
+            K_correct[j, i] = k_cor
+        
+        if np.all(is_tol_list[-stop_cond:]): 
+            break
+
+    return K_correct, k_old_list, k_cor_list, k_diff_list
 
 if __name__ == '__main__':
     kernel = np.load('saved/walk_kernel_3_subset_0.npy')
--- kernel_methods.py
+++ kernel_methods.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cvxpy as cp
+from cvxopt import matrix, solvers
 from data import load_training_data, load_test_data, split_data
 from kernel_class import Kernel_nwalk, RandomWalkKernel, KernelRBF
 from time import time
@@ -62,7 +63,7 @@ class KernelLogisticRegression:
 
 
 class KernelSVM(KernelMethod):
-    def __init__(self, lmbd=1., kernel_name='KernelRBF', precomputed_kernel=False, balanced = False, kernel_path='saved/', **kwargs):
+    def __init__(self, lmbd=1., balanced = False, precomputed_kernel=False, kernel_path='saved/', kernel_name='KernelRBF', **kwargs):
         super().__init__(kernel_name=kernel_name, **kwargs)
         self.lmbd = lmbd
         self.balanced = balanced
@@ -111,8 +112,7 @@ class KernelSVM(KernelMethod):
 
         print("Fitting KernelSVM")
         alpha = cp.Variable(N)
-        # obj = cp.Maximize(2 * alpha.T @ y - cp.quad_form(alpha, cp.psd_warp(K)))
-        obj = cp.Maximize(2 * alpha.T @ y - cp.quad_form(alpha, K))
+        obj = cp.Maximize(2 * alpha.T @ y - cp.quad_form(alpha, cp.psd_warp(K)))
         constraints = [0 <= cp.multiply(y, alpha), cp.multiply(y, alpha) <= weights / (2 * self.lmbd * N)]
         start = time()
         prob = cp.Problem(obj, constraints)
@@ -152,6 +152,165 @@ class KernelSVM(KernelMethod):
             return auc
         else:
             return np.sum(np.sign(y_pred) == y) / y.shape[0]
+
+
+class KernelSVM2():
+    def __init__(self, lmbd=1., balanced = False, kernel=None):
+        """
+        Args:
+            lmbd (float, optional): Penalisation parameter. Defaults to 1..
+            balanced (bool, optional): Whether we must weight samples inversely proportionally to the size of their class. Defaults to False.
+            kernel (Kernel, optional): Kernel object to use. Defaults to None.
+        """
+        self.lmbd = lmbd
+        self.balanced = balanced
+        self.kernel = kernel
+
+        self.X_support = None
+        self.X_mean = None
+        self.X_std = None
+
+        self.alpha = None
+        self.alpha_support = None
+    
+    def optimise(self, K, y, c, method="cvxopt"):
+        N = K.shape[0]
+        if method == "cvxpy":
+            print("Fitting KernelSVM")
+            alpha = cp.Variable(N)
+            obj = cp.Maximize(2 * alpha.T @ y - cp.quad_form(alpha, K))
+            constraints = [0 <= cp.multiply(y, alpha), cp.multiply(y, alpha) <= c]
+
+            start = time()
+            prob = cp.Problem(obj, constraints)
+            result = prob.solve()
+            end = time()
+
+            print(f'QP Solved in {end - start} secs')
+            return alpha.value
+        
+        else:
+            # min 1/2*(x^T)Qx + (p^T)x     st Gx <= h and Ax = b
+            P = matrix(K)
+            q = - matrix(y.astype(float))
+            G = matrix(np.concatenate([-np.diag(y), np.diag(y)], axis=0).astype(float))
+            h = matrix(np.concatenate([np.zeros(N), c]))
+            A = matrix(np.ones((1,N))) # if we want to take the version with b
+            # A = matrix(np.zeros((1,N)))
+            b = matrix(0.)
+            sol = solvers.qp(P, q, G, h, A, b)
+            return np.array(sol["x"]).flatten()
+    
+    def fit(self, K, y, X_train=None):
+        """Fit the SVM to the corresponding kernel matrix and labels
+
+        Args:
+            K (square ndarray): Kernel matrix
+            y (ndarray of dim 1): labels (-1 or 1)
+            X_train (ndarray of graphs, optional): Corresponding graphs. Not used except to save in an attribute, for future predictions. Defaults to None.
+        """
+        N = K.shape[0]
+        
+        if self.balanced:
+            N_pos = np.count_nonzero(y==1)
+            weights = np.where(y==1, N/(2*N_pos), N/(2*(N - N_pos)))
+        else:
+            weights = np.ones((N))
+
+        c = weights / (2 * self.lmbd * N)
+
+        self.alpha = self.optimise(K, y, c)
+        idx_alpha_support = np.nonzero(np.abs(self.alpha) > 10e-8)[0]
+        self.alpha_support = np.copy(self.alpha[idx_alpha_support])
+        if X_train is not None:
+            self.X_support = np.copy(X_train[idx_alpha_support])
+
+        idx_alpha_margin = np.nonzero((np.abs(self.alpha) > 1e-7) & (np.abs(self.alpha) < c - 1e-7))[0]
+        print(len(idx_alpha_support))
+        print(len(idx_alpha_margin))
+
+        b_candi = y - K[:,idx_alpha_support]@self.alpha_support # we could have said y - K@self.alpha, but implies errors
+        if len(idx_alpha_margin) > 0:
+            self.b = np.median(b_candi[idx_alpha_margin]) # offset of the classifier
+        else:
+            self.b = 0
+    
+    def fit_saved(self, kernel_path, y, X_train=None):
+        K = np.load(kernel_path)
+        return self.fit(K, y, X_train=X_train)
+    
+    def fit_compute(self, graph_list_train, y):
+        if self.kernel is None:
+            raise AttributeError("fit_compute needs attribute kernel to be difined")
+        
+        if self.kernel.name == "KernelRBF":
+            # If Kernel RBF, compute feature matrix
+            X_train = self.kernel.extract_features(graph_list_train)
+
+            # Normalizing features
+            self.X_mean = np.mean(X_train, axis=0)
+            self.X_std = np.std(X_train, axis=0)
+            self.X_std[np.argwhere(self.X_std == 0)] = 1e-8
+            X_train = (X_train - self.X_mean) / self.X_std
+
+        else:
+            X_train = graph_list_train
+        K = self.kernel.compute_gram_matrix(X_train)
+        return self.fit(K, y, X_train=X_train)
+    
+    def predict(self, K_outer):
+        if self.alpha is None:
+            raise AttributeError("The model has to be fitted first before predictions")
+        return K_outer @ self.alpha + self.b
+    
+    def predict_saved(self, outer_kernel_path = "saved/"):
+        K_outer = np.load(outer_kernel_path)
+        return self.predict(K_outer)
+    
+    def predict_compute(self, graph_list_test):
+        if self.kernel is None:
+            raise AttributeError("predict_compute needs attribute kernel to be difined.")
+        if self.X_support is None:
+            raise AttributeError("predict_compute needs X_train to have been filled in previously in the fit operation.")
+        if self.alpha_support is None:
+            raise AttributeError("The model has to be fitted first before predictions")
+        
+        if self.kernel.name == 'KernelRBF':
+            X_test = self.kernel.extract_features(graph_list_test)
+            X_test = (X_test - self.X_mean) / self.X_std
+
+        else:
+            X_test = graph_list_test
+
+        print("Computing kernel_outer")
+        kernel_outer = self.kernel.compute_outer_gram(X_test, self.X_support)
+        return kernel_outer @ self.alpha_support + self.b
+
+    def _score(self, y_true, y_pred, score_type='accuracy'):
+
+        if score_type == 'AUROC':
+            auc = np.count_nonzero(y_pred[y_true == 1][:,None] > y_pred[y_true != 1][None, :])
+            auc /= np.count_nonzero(y_true == 1)*np.count_nonzero(y_true != 1)
+            return auc
+        else:
+            return np.sum(np.sign(y_pred) == y_true) / y_true.shape[0]
+    
+    def score(self, K_outer, y_true, score_type='accuracy'):
+        y_pred = self.predict(K_outer)
+        return self._score(y_true, y_pred, score_type=score_type)
+    
+    def score_saved(self, outer_kernel_path, y_true, score_type='accuracy'):
+        y_pred = self.predict_saved(outer_kernel_path)
+        return self._score(y_true, y_pred, score_type=score_type)
+    
+    def score_compute(self, graph_list_test, y_true, score_type='accuracy'):
+        y_pred = self.predict_compute(graph_list_test)
+        return self._score(y_true, y_pred, score_type=score_type)
+    
+    
+
+
+
 
 
 if __name__ == '__main__':
